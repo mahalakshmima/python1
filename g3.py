9c7640b58fabb72055824a982b8fe4db146df19b
mahalakshmima <51949393+mahalakshmima@users.noreply.github.com>
2019-06-27 14:24:39 +0530

    Update g3.py

--- g3.py
+++ g3.py
@@ -1,8 +1,8 @@
 ch=input("enter the character:")
-if(ch=='A' or ch=='a' or ch=='E' or ch=='e' or ch=='I' or ch=="i" or ch=='O' or ch=='o' ch=='U' ch=='u' ):
-  print(ch,"is vowel")
-elif(ch=='B' or ch=='b' or ch=='C' or ch=='c' or ch=='D' ch=='d' ch=='F' or ch=='f' or ch=='G' or ch=='g' or ch=='H' or ch=='h' or ch=='J' or ch=='j' ch=='K' or ch=='k' or ch=='L' or ch=='l' or ch=='M' or ch=='m' or ch=='N' ch=='n' or ch=='P' or ch=='p' or ch=='Q' or ch=='q' or ch=='R' or ch=='r' ch=='S' or ch=='s' or ch=='T' or ch=='t' or ch=='V' or ch=='v' or ch=='W' or ch=='w' or ch=='X' or  ch=='x' or ch=='Y' or ch=="y' ch=='Z' ch=='z'):
-  print(ch,"is constant")
+if(ch=='A' or ch=='a' or ch=='E' or ch=='e' or ch=='I' or ch=="i" or ch=='O' or ch=='o' or ch=='U' or ch=='u' ):
+  print(ch,"is Vowel")
+elif(ch=='B' or ch=='b' or ch=='C' or ch=='c' or ch=='D' or  ch=='d' or  ch=='F' or ch=='f' or ch=='G' or ch=='g' or ch=='H' or ch=='h' or ch=='J' or ch=='j' or  ch=='K' or ch=='k' or ch=='L' or ch=='l' or ch=='M' or ch=='m' or ch=='N' or ch=='n' or ch=='P' or ch=='p' or ch=='Q' or ch=='q' or ch=='R' or ch=='r' ch=='S' or ch=='s' or ch=='T' or ch=='t' or ch=='V' or ch=='v' or ch=='W' or ch=='w' or ch=='X' or  ch=='x' or ch=='Y' or ch=='y' or ch=='Z' or ch=='z'):
+  print(ch,"is Constant")
 else:
   print("invalid")
 
