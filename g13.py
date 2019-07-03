3affa4b27bacab83c49641cc992f6f9e6dd8838d
mahalakshmima <51949393+mahalakshmima@users.noreply.github.com>
2019-07-03 16:09:40 +0530

    Update g13.py

--- g13.py
+++ g13.py
@@ -1,10 +1,10 @@
-m=int(input())
-if m>0:
-  for i in range(2,m):
-    if m%i==0:
-      print("no")
-  else:     
-      print("yes")
-else:
-  print("no")
-      
+b =int(input())
+if (b>1):   
+   for i in range(2, b//2):   
+       if (b%i) == 0: 
+           print("no")
+           break
+   else: 
+       print("yes") 
+else: 
+   print("no")
