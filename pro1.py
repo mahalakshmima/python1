2de7e9f6c4042a16f342fbf75a9122c9c0d48ea8
mahalakshmima <51949393+mahalakshmima@users.noreply.github.com>
2019-07-27 09:03:37 +0530

    Create pro1.py

---
+++ pro1.py
@@ -0,0 +1,15 @@
+bt1=int(input())
+xt1=[]
+for i in range(bt1):
+  at1=input()
+  xt1.append(at1)
+mv21=min(xt1,key=len)
+xt1.remove(mv21)
+for i in range(len(mv21)):
+  for j in range(len(xt1)):
+     cv21=xt[j]
+     if mv21[:i+1]==cv21[:i+1]:
+       result=mv21[:i+1]
+     else:
+       break
+print(result)
