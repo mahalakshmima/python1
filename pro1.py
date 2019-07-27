2d15ca28f05aae1e5cd65cd8fe25c018734df7fe
mahalakshmima <51949393+mahalakshmima@users.noreply.github.com>
2019-07-27 09:12:23 +0530

    Update pro1.py

--- pro1.py
+++ pro1.py
@@ -7,7 +7,7 @@ mv21=min(xt1,key=len)
 xt1.remove(mv21)
 for i in range(len(mv21)):
   for j in range(len(xt1)):
-     cv21=xt[j]
+     cv21=xt1[j]
      if mv21[:i+1]==cv21[:i+1]:
        result=mv21[:i+1]
      else:
