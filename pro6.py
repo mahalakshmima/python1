ad09ad87e11e3539576fa809af5bdaf1c42ec62f
mahalakshmima <51949393+mahalakshmima@users.noreply.github.com>
2019-07-27 09:45:20 +0530

    Create pro6.py

---
+++ pro6.py
@@ -0,0 +1,10 @@
+get1=int(input())
+set1=list(map(int,input().split()))
+go1=0
+for one12 in range(get1):
+    for two21 in range(one12,get1):  
+        for three33 in range(two21,get1):
+            if set1[one12]<set1[two21]<set1[three33]:
+                go1+=1
+print(go1)
+© 2019 GitHub, Inc.
