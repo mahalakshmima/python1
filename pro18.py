248de9089627c2ad4fdae3e9ba18e3c6a3edcaf5
mahalakshmima <51949393+mahalakshmima@users.noreply.github.com>
2019-08-06 19:56:06 +0530

    Create pro18.py

---
+++ pro18.py
@@ -0,0 +1,17 @@
+aprt1,brt1=map(int,input().split())
+l1t1=[]
+for _ in range(aprt1):
+    l1t1.append(input())
+for ic in range(len(l1t1)):
+    if('0' in l1t1[ic]):
+        l1t1[ic]=l1t1[ic].replace('0','')
+    l1t[ic]=l1t1[ic].replace(' ','')
+lengths=[]
+for ic in l1t1:
+    if(len(ic)>0):
+        lengths.append(len(ic))
+brt1=min(lengths)
+rt11='1 '*brt1
+rt11=rt11.strip()
+for ic in range(brt1):
+    print(rt11)
