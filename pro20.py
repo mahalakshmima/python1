7ba18e6e9fae998a2049470320832fdbd3fcf8a7
mahalakshmima <51949393+mahalakshmima@users.noreply.github.com>
2019-08-06 20:09:13 +0530

    Create pro20.py

---
+++ pro20.py
@@ -0,0 +1,11 @@
+nnnt1,mmmt1=list(map(int,input().split()))
+lt1=list(map(int,input().split()))
+lt1.sort(reverse=True)
+ct1=0
+for i in lt1:
+    if mmmt1==0:
+        break
+    qt1=mmmt1 // i
+    ct1+=qt1
+    mmmt=mmmt-i*qt1
+print(ct1)
