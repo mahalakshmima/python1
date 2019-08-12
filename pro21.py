f0a4b4d41b7c812f95b192c1dca5dc3fc6d681f4
mahalakshmima <51949393+mahalakshmima@users.noreply.github.com>
2019-08-12 20:39:15 +0530

    Update pro21.py

--- pro21.py
+++ pro21.py
@@ -1,8 +1,8 @@
 nt12=int(input())
 lt12=list(map(int,input().split()))[:nt12]
 div2=int(nt12/2)
-at12=sum(lt12[:div])//len(lt12[:div])
-at22=sum(lt12[div:])//len(lt12[div:])
+at12=sum(lt12[:div2])//len(lt12[:div2])
+at22=sum(lt12[div2:])//len(lt12[div2:])
 if(at12==at22):
   print("yes")
 else:
