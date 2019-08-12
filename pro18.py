86070dd62c379aee2396cad856d866d3deee2e9c
mahalakshmima <51949393+mahalakshmima@users.noreply.github.com>
2019-08-12 20:14:15 +0530

    Update pro18.py

--- pro18.py
+++ pro18.py
@@ -6,11 +6,11 @@ for ic in range(len(l1t1)):
     if('0' in l1t1[ic]):
         l1t1[ic]=l1t1[ic].replace('0','')
     l1t[ic]=l1t1[ic].replace(' ','')
-lengths=[]
+lengths1=[]
 for ic in l1t1:
     if(len(ic)>0):
-        lengths.append(len(ic))
-brt1=min(lengths)
+        lengths1.append(len(ic))
+brt1=min(lengths1)
 rt11='1 '*brt1
 rt11=rt11.strip()
 for ic in range(brt1):
