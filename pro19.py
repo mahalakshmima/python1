3bee3e2e27bc6d430f42e9c5194abf83a9a5a598
mahalakshmima <51949393+mahalakshmima@users.noreply.github.com>
2019-08-12 20:27:53 +0530

    Update pro19.py

--- pro19.py
+++ pro19.py
@@ -1,11 +1,10 @@
 size81=int(input())
 arr31=[]
 for i in range(size81):
-
-	      st1=input()
-	      st1=list(map(int,st1.split(" ")))
-	      lt1=len(st1)
-	      for j in range(lt1):
-		            arr3.append(st1[j])
+ 	st1=input()
+	st1=list(map(int,st1.split(" ")))
+	lt1=len(st1)
+	for j in range(lt1):
+		arr3.append(st1[j])
 arr31.sort()
 print(*arr31,sep=" ") 
