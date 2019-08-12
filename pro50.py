06ca9f65330ec78806456dd2a45ebe91e9ecde93
mahalakshmima <51949393+mahalakshmima@users.noreply.github.com>
2019-08-12 13:44:56 +0530

    Update pro50.py

--- pro50.py
+++ pro50.py
@@ -1,24 +1,24 @@
-xt,yt=map(int,input().split())
-zt=[]
+xt1,yt1=map(int,input().split())
+zt1=[]
 
-for i in range(yt):
-  zt.append(list(map(int,input().split())))
-maht=10000000
-bht=0
+for i in range(yt1):
+  zt1.append(list(map(int,input().split())))
+maht1=10000000
+bht1=0
 
-for i in range(yt):
-  if zt[i][0]==1:
-    sont=zt[i][1]
-    pict=zt[i][2]
-    for j in range(i+1,yt):
-      if zt[j][0]==sont:
-        sont=zt[j][1]
-        pict=pict+zt[j][2]
-    if pict<maht and sont==xt:
-      maht=pict
-      bht=bht+1
+for i in range(yt1):
+  if zt1[i][0]==1:
+    sont=zt1[i][1]
+    pict=zt1[i][2]
+    for j in range(i+1,yt1):
+      if zt1[j][0]==sont1:
+        sont1=zt1[j][1]
+        pict1=pict1+zt1[j][2]
+    if pict1<maht1 and sont1==xt1:
+      maht1=pict1
+      bht1=bht1+1
 
-if bht==0:
+if bht1==0:
   print(-1)
 else:
-print(maht)
+print(maht1)
